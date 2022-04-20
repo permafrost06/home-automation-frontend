80cf30ac218f631dd32626afb7daa842899b332b
permafrost06 <zahin06@gmail.com>
2022-04-21 02:10:33 +0600

    added basic signup page

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -1,3 +1,49 @@
 <template>
-  <div></div>
+  <div>
+    <form>
+      <p>Name: <input v-model="name" type="text" /></p>
+      <p>
+        Phone Code:
+        <select v-model="phoneCode">
+          <option value="+91">+91</option>
+        </select>
+      </p>
+      <p>Phone: <input v-model="phone" type="number" /></p>
+      <p>Email: <input v-model="email" type="email" /></p>
+      <p>Password: <input v-model="password" type="password" /></p>
+      <div>
+        <button @click.stop.prevent="handleSignUp">Sign Up</button>
+      </div>
+      <p>
+        Already have an account?
+        <router-link :to="{ name: 'Login' }">Log in here.</router-link>
+      </p>
+    </form>
+  </div>
 </template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  setup() {
+    const name = ref("");
+    const phoneCode = ref("");
+    const phone = ref<number>();
+    const email = ref("");
+    const password = ref("");
+
+    const handleSignUp = () => {
+      console.log(
+        name.value,
+        phoneCode.value,
+        phone.value,
+        email.value,
+        password.value
+      );
+    };
+
+    return { name, phoneCode, phone, email, password, handleSignUp };
+  },
+});
+</script>
