f925e5122e6264a25117d5ddc49d828022234b67
permafrost06 <zahin06@gmail.com>
2022-04-21 01:45:57 +0600

    basic login page added

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,12 +1,35 @@
 <template>
-  <div></div>
+  <div>
+    <form>
+      <div>
+        <input v-model="email" type="text" placeholder="Email" />
+        <input v-model="password" type="password" placeholder="Passowrd" />
+      </div>
+      <div>
+        <button @click="logIn">Log in</button>
+      </div>
+    </form>
+    <p>
+      Don`t have an account ?
+      <router-link :to="{ name: 'Signup' }">Sign Up here.</router-link>
+    </p>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
 
 export default defineComponent({
   name: "Login",
-  components: {},
+  setup() {
+    const email = ref("");
+    const password = ref("");
+
+    const logIn = () => {
+      console.log(email.value, password.value);
+    };
+
+    return { email, password, logIn };
+  },
 });
 </script>
