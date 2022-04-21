a751a8452903bdeed7ffe10f8cb5a9a474f4ebef
permafrost06 <zahin06@gmail.com>
2022-04-22 01:23:18 +0600

    added new device types

--- src/models/device.model.ts
+++ src/models/device.model.ts
@@ -1,7 +1,7 @@
 export interface Device {
   id: string;
   name: string;
-  type: "television" | "fridge";
+  type: "television" | "fridge" | "fan" | "aircon";
   isDeviceOn?: boolean;
   lastTurnedOn?: Date;
   lastTurnedOff?: Date;
