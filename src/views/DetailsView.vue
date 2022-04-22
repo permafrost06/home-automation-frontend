2c96f1d565c037c6831d90b78454fb509e6f0b37
permafrost06 <zahin06@gmail.com>
2022-04-23 01:09:32 +0600

    styled details view

--- src/views/DetailsView.vue
+++ src/views/DetailsView.vue
@@ -7,49 +7,78 @@ const devicesStore = useDevicesStore();
 const devices = devicesStore.devices as Array<Device>;
 
 const readableTime = (time: Date | undefined) => {
-  if (!time) return;
+  if (!time) return "No data";
   return time.toLocaleTimeString();
 };
 </script>
 
 <template>
-  <div class="table">
-    <router-link :to="{ name: 'Dashboard' }">Go to Dashboard</router-link>
-    <h1>Devices</h1>
-    <div class="row header">
-      <div class="col col-1">Device Name</div>
-      <div class="col col-2">Last turned on</div>
-      <div class="col col-3">Last turned off</div>
-      <div class="col col-4">Usage</div>
-    </div>
-    <div class="row" v-for="device in devices" :key="device.id">
-      <div class="col col-1">{{ device.name }}</div>
-      <div class="col col-2">{{ readableTime(device.lastTurnedOn) }}</div>
-      <div class="col col-3">{{ readableTime(device.lastTurnedOff) }}</div>
-      <div class="col col-4">{{ device.usage }}</div>
+  <div class="table-container">
+    <div class="table">
+      <h1 style="color: var(--color-light-1)">Devices</h1>
+      <div class="cells">
+        <div class="row header">
+          <div class="col col-1">Device Name</div>
+          <div class="col col-2">Last turned on</div>
+          <div class="col col-3">Last turned off</div>
+          <div class="col col-4">Usage</div>
+        </div>
+        <div class="row" v-for="device in devices" :key="device.id">
+          <div class="col col-1">{{ device.name }}</div>
+          <div class="col col-2">{{ readableTime(device.lastTurnedOn) }}</div>
+          <div class="col col-3">{{ readableTime(device.lastTurnedOff) }}</div>
+          <div class="col col-4">{{ device.usage }} units</div>
+        </div>
+      </div>
+      <router-link style="text-align: center" :to="{ name: 'Dashboard' }">
+        <button>Go to Dashboard</button>
+      </router-link>
     </div>
   </div>
 </template>
 
-<style>
-.col {
-  display: inline-block;
-}
+<style lang="scss">
+.table {
+  width: 37rem;
+  margin-inline: auto;
+  text-align: center;
 
-.col-1 {
-  width: 10rem;
-}
+  .col {
+    display: inline-block;
+    text-align: center;
+    padding: 0.25rem;
+  }
 
-.col-2,
-.col-3 {
-  width: 8rem;
-}
+  .col-1 {
+    width: 10rem;
+    border-right: 1px solid var(--color-med-1);
+  }
 
-.col-4 {
-  width: 3rem;
-}
+  .col-2,
+  .col-3 {
+    width: 10rem;
+    border-right: 1px solid var(--color-med-1);
+  }
 
+  .col-4 {
+    width: 5rem;
+  }
+
+  .row {
+    width: 100%;
+  }
+
+  .header {
+    border-bottom: 1px solid var(--color-med-1);
+  }
+}
+.cells {
+  padding: 1rem;
+  background: var(--color-light-1);
+  border-radius: 1rem;
+}
 .table .header .col {
   font-weight: 700;
+  font-size: 1.2rem;
 }
 </style>
