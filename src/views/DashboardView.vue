88f54bcea58ef9327130c67ce025b3c11cfee0b5
permafrost06 <zahin06@gmail.com>
2022-04-23 01:10:12 +0600

    temorarily disable debug data gen

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -5,28 +5,28 @@ import DeviceComponent from "../components/DeviceComponent.vue";
 const store = useDevicesStore();
 
 // debug mock data automatically generated
-if (!store.devices.length) {
-  store.createNewDevice({
-    id: "1",
-    name: "LivingRoom TV",
-    type: "television",
-  });
-  store.createNewDevice({
-    id: "2",
-    name: "Kitchen Fridge",
-    type: "fridge",
-  });
-  store.createNewDevice({
-    id: "3",
-    name: "Bedroom AC",
-    type: "aircon",
-  });
-  store.createNewDevice({
-    id: "4",
-    name: "Guest Room Fan",
-    type: "fan",
-  });
-}
+// if (!store.devices.length) {
+//   store.createNewDevice({
+//     id: "1",
+//     name: "LivingRoom TV",
+//     type: "television",
+//   });
+//   store.createNewDevice({
+//     id: "2",
+//     name: "Kitchen Fridge",
+//     type: "fridge",
+//   });
+//   store.createNewDevice({
+//     id: "3",
+//     name: "Bedroom AC",
+//     type: "aircon",
+//   });
+//   store.createNewDevice({
+//     id: "4",
+//     name: "Guest Room Fan",
+//     type: "fan",
+//   });
+// }
 </script>
 
 <template>
