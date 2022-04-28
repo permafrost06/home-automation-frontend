e495d945b3866c33649efd7d96e04e9795c52a40
permafrost06 <zahin06@gmail.com>
2022-04-28 16:35:08 +0600

    added styles

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,10 +4,37 @@
     <p>username</p>
   </header>
   <div class="container-holder">
-    <p>You can monitor and control all your home devices and appliances</p>
-    <p>
-      All data are stored in a decentralized blockchain so that you don't lose
-      any of the information and no perpetrator can steal data from you
-    </p>
+    <div class="text-container">
+      <p class="home top">
+        You can monitor and control all your home devices and appliances
+      </p>
+      <p class="home">
+        All data are stored in a decentralized blockchain so that you don't lose
+        any of the information and no perpetrator can steal data from you
+      </p>
+    </div>
   </div>
 </template>
+
+<style scoped>
+.text-container {
+  margin-top: 15rem;
+  margin-inline: auto;
+  padding: 1rem;
+
+  border-radius: 9px;
+
+  width: 60rem;
+
+  background-color: var(--color-dark-1);
+}
+p.home {
+  text-align: center;
+  font-size: 1.5rem;
+  color: var(--color-light-1);
+}
+
+p.top {
+  margin-bottom: 3rem;
+}
+</style>
